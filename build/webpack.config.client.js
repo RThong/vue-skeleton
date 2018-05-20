17fd5e46fc408b9b391fe5645faacf734617acb9
rthong <403216075@qq.com>
2018-05-21 00:02:46 +0800

    home

--- build/webpack.config.client.js
+++ build/webpack.config.client.js
@@ -4,7 +4,22 @@ const HTMLplugin = require('html-webpack-plugin')
 const path = require('path')
 
 const isDev = process.env.NODE_ENV === 'development'
-console.log(process.env.NODE_ENV)
+
+const devServer = {
+	port: 8080,
+  host: '0.0.0.0',
+  overlay: {
+    errors: true,
+  },
+  // headers: {
+  //   'Access-Control-Allow-Origin': '*'
+  // },
+  // historyApiFallback: {
+  //   index: '/public/index.html'
+  // },//history模式手动刷新url会发送请求，出现错误
+  hot: true
+}
+
 let config
 
 config = {
@@ -20,29 +35,30 @@ config = {
 				test: /\.vue$/,
 				loader: 'vue-loader'
 			},
-			// {
-			// 	test: /\.css$/,
-			// 	use: [
-			// 	'style-loader',
-			// 	'css-loader'
-			// 	] 
-			// }
 			{
-				test: /\.css$/,
-				loader: 'style-loader!css-loader'
+				test: /\.sass$/,
+				use: [
+					'style-loader',
+					'css-loader',
+					{
+						loader: 'postcss-loader',
+            options: {
+              sourceMap: true,
+            }
+					},
+					'sass-loader'
+				]
 			}
 		]
 	},
+	devServer,
 	plugins: [
 		new VueLoaderPlugin(),
 		new HTMLplugin({
 	    template: path.join(__dirname, '../index.html')
-	  })
-	// 	new webpack.DefinePlugin({
- //  		'process.env': {
- //  			NODE_ENV: isDev ? '"development"': '"production"'
- //  		}
- //  	})
+	  }),
+		new webpack.HotModuleReplacementPlugin(),
+		new webpack.NoEmitOnErrorsPlugin()
 	]
 }
 
--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -12,8 +12,17 @@
 		}
 	}
 </script>
-<style scoped>
+<style lang="sass" scoped>
 	.test{
 		color: red;
+		width: 100px;
+		height: 50px;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		border-radius: 10px;
+		background: #ccc;
+		box-shadow: inset 0 -2px 1px rgba(0,0,0,.03);
+		transform: translateX(50%);
 	}
 </style>
